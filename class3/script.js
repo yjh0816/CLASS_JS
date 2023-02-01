3de67d07f2e73837039b9370419f3158ddca4439
yjh0816 <2543kt@naver.com>
2023-02-01 16:54:48 +0900

    feat: Update lecture 8

--- class3/script.js
+++ class3/script.js
@@ -507,86 +507,126 @@ function appendText(text, callback) {
 // }
 
 // practice
-async function getPost(id) {
+// async function getPost(id) {
+//   let response = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${id}`,
+//   );
+//   let data = await response.json();
+//   return data;
+// }
+
+// async function updatePost(post, id) {
+//   console.log(post);
+//   let response = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${id}`,
+//     {
+//       method: 'PUT',
+//       body: JSON.stringify(post),
+//       headers: {
+//         'Content-type': 'application/json',
+//       },
+//     },
+//   );
+//   let data = await response.json();
+//   return data;
+// }
+
+// async function getPosts() {
+//   let response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   let data = await response.json();
+//   return data;
+// }
+
+// async function deletePost(id) {
+//   console.log(id);
+//   let response = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${id}`,
+//     {
+//       method: 'DELETE',
+//     },
+//   );
+//   let data = await response.json();
+//   return data;
+// }
+
+// async function erasePost(id) {
+//   let data = await deletePost(id);
+//   let div = document.getElementById(`${id}`);
+//   div.remove();
+// }
+// async function showPosts() {
+//   let data = await getPosts();
+//   data.forEach(element => {
+//     document.body.insertAdjacentHTML(
+//       'beforeBegin',
+//       `<div id="${element.id}"><h2>${element.title}</h2><br><p>${element.body}</p><button onclick="erasePost(${element.id})">삭제</button></div>`,
+//     );
+//   });
+// }
+
+// async function getOnePost(id) {
+//   let data = await getPost(id);
+//   let title = document.getElementById('title');
+//   let body = document.getElementById('body');
+//   title.value = data.title;
+//   body.value = data.body;
+// }
+
+// async function modifyPost(id) {
+//   let title = document.getElementById('title').value;
+//   let body = document.getElementById('body').value;
+//   let post = {
+//     title: title,
+//     body: body,
+//   };
+//   let data = await updatePost(post, id);
+//   console.log(data);
+//   let div = document.getElementById(`${id}`);
+//   div.innerHTML = `<div id="${data.id}"><h2>${data.title}</h2><br><p>${data.body}</p><button onclick="erasePost(${data.id})">삭제</button></div>`;
+// }
+
+// showPosts();
+// getOnePost(1);
+
+// let button = document.querySelectorAll('button')[0];
+// button.onclick = () => modifyPost(1);
+
+// practice
+
+const LOCATION = {
+  london: {
+    latitude: 51.5,
+    longitude: -0.120000124,
+  },
+  newyork: {
+    latitude: 51.5,
+    longitude: -0.120000124,
+  },
+  seoul: {
+    latitude: 37.57,
+    longitude: 126.98,
+  },
+};
+
+function setTime(event) {
+  showInfo(event.target.id);
+}
+
+async function getInfo(country) {
+  console.log(LOCATION[country].latitude);
+
   let response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    `https://api.open-meteo.com/v1/forecast?latitude=${LOCATION[country].latitude}&longitude=${LOCATION[country].longitude}&daily=weathercode&current_weather=true&timezone=auto`,
   );
   let data = await response.json();
   return data;
 }
 
-async function updatePost(post, id) {
-  console.log(post);
-  let response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`,
-    {
-      method: 'PUT',
-      body: JSON.stringify(post),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    },
-  );
-  let data = await response.json();
-  return data;
-}
-
-async function getPosts() {
-  let response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  let data = await response.json();
-  return data;
-}
-
-async function deletePost(id) {
-  console.log(id);
-  let response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`,
-    {
-      method: 'DELETE',
-    },
-  );
-  let data = await response.json();
-  return data;
-}
-
-async function erasePost(id) {
-  let data = await deletePost(id);
-  let div = document.getElementById(`${id}`);
-  div.remove();
-}
-async function showPosts() {
-  let data = await getPosts();
-  data.forEach(element => {
-    document.body.insertAdjacentHTML(
-      'beforeBegin',
-      `<div id="${element.id}"><h2>${element.title}</h2><br><p>${element.body}</p><button onclick="erasePost(${element.id})">삭제</button></div>`,
-    );
-  });
-}
-
-async function getOnePost(id) {
-  let data = await getPost(id);
-  let title = document.getElementById('title');
-  let body = document.getElementById('body');
-  title.value = data.title;
-  body.value = data.body;
-}
-
-async function modifyPost(id) {
-  let title = document.getElementById('title').value;
-  let body = document.getElementById('body').value;
-  let post = {
-    title: title,
-    body: body,
-  };
-  let data = await updatePost(post, id);
+async function showInfo(country) {
+  let data = await getInfo(country);
   console.log(data);
-  let div = document.getElementById(`${id}`);
-  div.innerHTML = `<div id="${data.id}"><h2>${data.title}</h2><br><p>${data.body}</p><button onclick="erasePost(${data.id})">삭제</button></div>`;
+  document.body.insertAdjacentHTML(
+    'beforeEnd',
+    `<div><h2>${data.current_weather.weathercode}</h2><br><p>${data.current_weather.time}</p><br><p>"${data.current_weather.temperature}"</p></div>`,
+  );
 }
-
-showPosts();
-getOnePost(1);
-
-let button = document.querySelectorAll('button')[0];
-button.onclick = () => modifyPost(1);
